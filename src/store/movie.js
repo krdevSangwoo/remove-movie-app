fddf92a0950f1222d56d21d91110b7d0149bcd02
krdevSangwoo <tkddn8998@gmail.com>
2022-12-27 22:53:37 +0900

    Add Serverless functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -19,9 +19,14 @@ export const searchMovies = async (page) => {
     store.state.message = "";
   }
   try {
-    const res = await fetch(
-      `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
-    );
+    // 서버리스 함수로 통신하여 apikey 보호
+    const res = await fetch("/api/movie", {
+      method: "POST",
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page,
+      }),
+    });
     const { Search, totalResults, Response, Error } = await res.json();
     if (Response === "True") {
       store.state.movies = [...store.state.movies, ...Search];
@@ -38,9 +43,12 @@ export const searchMovies = async (page) => {
 
 export const getMovieDetails = async (id) => {
   try {
-    const res = await fetch(
-      `https://omdbapi.com?apikey=7035c60c&i=${id}&plot=full`
-    );
+    const res = await fetch("/api/movie", {
+      method: "POST",
+      body: JSON.stringify({
+        id,
+      }),
+    });
     store.state.movie = await res.json();
   } catch (error) {
     console.log("getMovieDetails error: ", error);
